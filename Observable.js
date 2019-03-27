4dd10c5b4d26e53158926b9095a46329f801747a
kaiser <765934690@qq.com>
2019-03-27 19:27:16 +0800

    fist

--- Observable.js
+++ Observable.js
@@ -1,38 +1,48 @@
 /*
- * @Author: kael 
- * @Date: 2018-02-01 17:41:25 
- * @Last Modified by: kael
- * @Last Modified time: 2018-02-02 17:38:36
+ * @Author: kael
+ * @Date: 2018-02-01 17:41:25
+ * @Last Modified by: Kaiser
+ * @Last Modified time: 2019-03-27 19:26:51
  */
 
 class ObserverList {
   constructor() {
-    this.observerList = [];
+    this.observerList = []
   }
   add(observer) {
     // todo add observer to list
+    this.observerList.push(observer)
   }
   remove(observer) {
     // todo remove observer from list
+    const index = this.observerList.indexOf(observer)
+    if (index === -1) return
+    this.observerList.splice(index, 1)
   }
   count() {
     // return observer list size
+    return this.observerList.length
   }
 }
 
 class Subject {
   constructor() {
-    this.observers = new ObserverList();
+    this.observers = new ObserverList()
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer)
   }
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove(observer)
   }
   notify(...args) {
     // todo notify
+    this.observers.observerList.forEach(observer => {
+      observer.update(...args)
+    })
   }
 }
 
-module.exports = { Subject };
+module.exports = { Subject }
--- PubSub.js
+++ PubSub.js
@@ -1,26 +1,29 @@
 /*
- * @Author: kael 
- * @Date: 2018-02-01 17:41:25 
- * @Last Modified by: kael
- * @Last Modified time: 2018-02-02 17:39:45
+ * @Author: kael
+ * @Date: 2018-02-01 17:41:25
+ * @Last Modified by: Kaiser
+ * @Last Modified time: 2019-03-27 19:21:45
  */
 
 module.exports = class PubSub {
-
   constructor() {
-    this.subscribers = {};
+    this.subscribers = {}
   }
 
   subscribe(type, fn) {
     // todo subscribe
+    const that = this
+    that.subscribers[type] = fn
   }
 
   unsubscribe(type, fn) {
     // todo unsubscribe
+    const that = this
+    that.subscribers[type] = undefined
   }
 
   publish(type, ...args) {
     // todo publish
+    this.subscribers[type] && this.subscribers[type](...args)
   }
-
 }
